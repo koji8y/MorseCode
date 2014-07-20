16251d7010a548f414c920dfa8129d8a567f0764
Koji Yamamoto <koji@pobox.com>
2014-07-21 03:42:14 +0900

    now litName and blink is selected by input pin voltage

--- MorseCode/MorseCode.c
+++ MorseCode/MorseCode.c
@@ -11,9 +11,11 @@
 
 #define MORSE_UNIT 250L // 250ms
 
-# define MORSE_DDR DDRB
-# define MORSE_PORT PORTB
-# define MORSE_PIN PINB4
+#define MORSE_DDR DDRB
+#define MORSE_PORT PORTB
+#define MORSE_OUTPINBIT PINB4
+#define SELECT_PIN PINB
+#define SELECT_INPINBIT PB3
 
 void inCodeBreak()
 {
@@ -30,9 +32,9 @@ void plusWordBreak()
 
 #define tone(count) \
 { \
-	MORSE_PORT |= _BV(MORSE_PIN); \
+	MORSE_PORT |= _BV(MORSE_OUTPINBIT); \
 	_delay_ms(MORSE_UNIT * count); \
-	MORSE_PORT &= ~_BV( MORSE_PIN); \
+	MORSE_PORT &= ~_BV( MORSE_OUTPINBIT); \
 	inCodeBreak(); \
 }
 void shortTone()
@@ -46,16 +48,16 @@ void longTone()
 
 //void shortTone()
 //{
-	//MORSE_PORT |= (1 << MORSE_PIN);
+	//MORSE_PORT |= (1 << MORSE_OUTPINBIT);
 	//_delay_ms(MORSE_UNIT);
-	//MORSE_PORT &= !(1 << MORSE_PIN);
+	//MORSE_PORT &= !(1 << MORSE_OUTPINBIT);
 	//inCodeBreak();
 //}
 //void longTone()
 //{
-	//MORSE_PORT |= (1 << MORSE_PIN);
+	//MORSE_PORT |= (1 << MORSE_OUTPINBIT);
 	//_delay_ms(MORSE_UNIT * 3L);
-	//MORSE_PORT &= !(1 << MORSE_PIN);
+	//MORSE_PORT &= !(1 << MORSE_OUTPINBIT);
 	//inCodeBreak();
 //}
 
@@ -74,28 +76,42 @@ void charY()
 	longTone();
 }
 
-int main(void)
+void litName()
 {
-	MORSE_DDR = 0xff;
-	MORSE_PORT = 0x00;
-	_delay_ms(2000);
-	/*
-	int times = 10;
-	//while(1)
-	while (times > 0)
-	{
-		MORSE_PORT ^= (1 << MORSE_PIN);
-		_delay_ms(1000);
-		times--;
-	}
-	*/
 	charY();
 	plusCharBreak();
 	charK();
 	plusWordBreak();
 	charY();
 	plusCharBreak();
-	charK();
+	charK();	
+}
 
-	MORSE_PORT &= ~_BV(MORSE_PIN);
+void blink()
+{
+	int times = 10;
+	//while(1)
+	while (times > 0)
+	{
+		MORSE_PORT ^= (1 << MORSE_OUTPINBIT);
+		_delay_ms(1000);
+		times--;
+	}
+}
+
+int main(void)
+{
+	MORSE_DDR = 0xff ^ _BV(SELECT_INPINBIT); // set for output other than SELECT_INPINBIT
+	MORSE_PORT = _BV(SELECT_INPINBIT); // configure pull-up for SELECT_INPINBIT
+	_delay_ms(2000);
+	if (bit_is_clear(SELECT_PIN, SELECT_INPINBIT)) {
+		blink();
+	} else {
+		litName();
+	}
+	//blink();
+	//_delay_ms(2000);
+	//litName();
+
+	MORSE_PORT &= ~_BV(MORSE_OUTPINBIT);
 }
